301d30bfdab176193b2dc5e3c651aaafe05a0ca0
zackckpetersen <zackcpetersen@gmail.com>
2020-12-30 13:08:19 -0700

    building status, project, and user filter

--- src/components/entries/index.vue
+++ src/components/entries/index.vue
@@ -1,5 +1,31 @@
 <template>
     <v-row justify="center">
+        <v-col cols="12" md="10">
+            <v-row justify="space-around" class="px-5">
+                <v-col cols="10" sm="6" md="4" lg="3">
+                    <update-date v-model="startDate" label="Start Date" icon="mdi-calendar"></update-date>
+                    <v-spacer></v-spacer>
+                </v-col>
+                <v-col cols="10" sm="6" md="4" lg="3">
+                    <update-date v-model="endDate" label="End Date" icon="mdi-calendar"></update-date>
+                </v-col>
+
+                <v-col cols="10" sm="6" md="4" lg="3">
+                    <project-filter v-model="projectList"></project-filter>
+                    <v-spacer></v-spacer>
+                </v-col>
+                <v-col cols="10" sm="6" md="4" lg="3">
+                    <status-filter v-model="statusList"></status-filter>
+                    <v-spacer></v-spacer>
+                </v-col>
+                <v-col cols="10" sm="6" md="4" lg="3">
+                    <user-filter v-model="userList"></user-filter>
+                    <v-spacer></v-spacer>
+            </v-col>
+            </v-row>
+        </v-col>
+        <v-col cols="12">
+        <v-row justify="center">
         <v-card class="rounded-lg" max-width="95%">
             <v-card-title>
                 <v-col cols="12" md="6">
@@ -61,13 +87,19 @@
         </v-card>
         <edit-entry :showModal="entryEditModal" @status="editModalStatus"></edit-entry>
     </v-row>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
 import { mapMutations } from 'vuex'
-import entryStatusUpdate from '@/components/entries/entryStatusUpdate'
-import entryConstants from '@/constants/entries'
 import editEntry from '@/components/entries/edit'
+import entryConstants from '@/constants/entries'
+import entryStatusUpdate from '@/components/entries/entryStatusUpdate'
+import projectFilter from '@/components/entries/projectFilter'
+import statusFilter from '@/components/entries/statusFilter'
+import updateDate from '@/components/entries/updateDate'
+import userFilter from '@/components/entries/userFilter'
 
 export default {
     data () {
@@ -77,7 +109,12 @@ export default {
             selected: [],
             entryEditModal: false,
             dialogDelete: false,
-            userEntries: []
+            userEntries: [],
+            projectList: [],
+            statusList: [],
+            userList: [],
+            startDate: null,
+            endDate: null,
         }
     },
     methods: {
@@ -107,19 +144,42 @@ export default {
         },
         clearStatus () {
             this.selected = []
+        },
+        formattedDate (numDays) {
+            const today = new Date()
+            today.setDate(today.getDate() - numDays)
+            return today.toLocaleDateString('en-CA')
         }
     },
     computed: {
         headers () {
             return [
                 { text: 'User', value: 'user', align: 'start' },
-                { text: 'Date', value: 'date' },
+                {
+                    text: 'User Id', value: 'userId', align: ' d-none', filter: user => {
+                        return this.userList.length ? this.userList.includes(user) : true
+                    }
+                },
+                {
+                    text: 'Date', value: 'date', filter: date => {
+                        return date >= this.startDate && this.endDate >= date
+                    }
+                },
                 { text: 'Start Time', value: 'start_time', sortable: false },
                 { text: 'End Time', value: 'end_time', sortable: false },
                 { text: 'Time Paused', value: 'time_paused' },
                 { text: 'Time Worked', value: 'time_worked' },
                 { text: 'Project', value: 'project' },
-                { text: 'Status', value: 'status' },
+                {
+                    text: 'Project Id', value: 'project_id', align: ' d-none', filter: project => {
+                        return this.projectList.length ? this.projectList.includes(project) : true
+                    }
+                },
+                {
+                    text: 'Status', value: 'status', filter: status => {
+                        return this.statusList.length ? this.statusList.includes(status) : true
+                    }
+                },
                 { text: 'Created', value: 'created_at' },
                 { text: 'Updated', value: 'updated_at' },
                 { text: 'Actions', value: 'actions', sortable: false, filterable: false}
@@ -133,12 +193,14 @@ export default {
                 const entryDate = new Date(entry.start_time)
                 formattedEntry['id'] = entry.id
                 formattedEntry['user'] = this.fullName(entry)
-                formattedEntry['date'] = entryDate.toLocaleDateString()
+                formattedEntry['userId'] = entry.user.id
+                formattedEntry['date'] = entryDate.toLocaleDateString('en-CA')
                 formattedEntry['start_time'] = entryDate.toLocaleTimeString()
                 formattedEntry['end_time'] = entry.end_time ? new Date(entry.end_time).toLocaleTimeString() : null
                 formattedEntry['time_paused'] = this.durationFormatted(entry.time_paused_secs)
                 formattedEntry['time_worked'] = this.durationFormatted(entry.time_worked_secs)
                 formattedEntry['project'] = entry.project_name
+                formattedEntry['project_id'] = entry.project
                 formattedEntry['status'] = entry.status
                 formattedEntry['created_at'] = this.dateTime(entry.created_at)
                 formattedEntry['updated_at'] = this.dateTime(entry.updated_at)
@@ -152,8 +214,16 @@ export default {
         entries: Array
     },
     components: {
+        'edit-entry': editEntry,
         'entry-status': entryStatusUpdate,
-        'edit-entry': editEntry
+        'project-filter': projectFilter,
+        'status-filter': statusFilter,
+        'update-date': updateDate,
+        'user-filter': userFilter
+    },
+    created () {
+        this.startDate = this.formattedDate(14)
+        this.endDate = this.formattedDate(0)
     }
 }
 </script>
---
+++ src/components/entries/statusFilter.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-autocomplete
+        v-model="statusList"
+        :items="statuses"
+        item-text="name"
+        item-value="id"
+        multiple
+        label="Status"
+    >
+        <template v-slot:prepend>
+            <v-icon>mdi-check-box-outline</v-icon>
+        </template>
+    </v-autocomplete>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            statusList: [],
+            statuses: [
+                { name: 'Active', id: 'active' },
+                { name: 'Approved', id: 'approved' },
+                { name: 'Needs Approval', id: 'needs_approval' },
+                { name: 'Flagged', id: 'flagged' },
+            ],
+        }
+    },
+    watch: {
+        statusList () {
+            this.$emit('input', this.statusList)
+        }
+    },
+    props: {
+        value: null
+    }
+}
+</script>
